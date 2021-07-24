9954b91094a149ea33a4e36c7053ee1578da105d
NekoSina <nekosalad@protonmail.com>
2021-07-24 16:29:15 +0430

    knight and bishop done

--- Services/ChessLogic.cs
+++ Services/ChessLogic.cs
@@ -21,7 +21,13 @@ namespace ChessWebsite.DTOs
                     break;    
                     case 'Q':
                         CalculateQueenMoves(piece);  
-                    break;              
+                    break;       
+                    case 'N':
+                        CalculateKnightMoves(piece);
+                    break;
+                    case 'B':
+                        CalculateBishopMoves(piece);
+                    break;
                     default:
                     break;
                 }        
@@ -149,11 +155,11 @@ namespace ChessWebsite.DTOs
                     for(int length = 1; length<9 ; length++)
                     if(r != 0 || f != 0)
                     {
-                        var newr = queen.Rank + r*length;
-                        var newf = queen.File + f*length;
-                        if(newr >= 0 && newr <8 &&  newf>=0 && newf<8)
+                        var targetrank = queen.Rank + r*length;
+                        var targetfile = queen.File + f*length;
+                        if(targetrank >= 0 && targetrank <8 &&  targetfile>=0 && targetfile<8)
                         {
-                            targetsquare = Board.GetSquareByRankAndFile(newr, newf);
+                            targetsquare = Board.GetSquareByRankAndFile(targetrank, targetfile);
                             if (targetsquare.OccupingPiece!= "")
                              {
                                  if(targetsquare.OccupingPiece [0] != queen.OccupingPiece[0])
@@ -168,11 +174,51 @@ namespace ChessWebsite.DTOs
         }
         private void CalculateBishopMoves(Square bishop)
         {
+            Square targetsquare;
+            for(int f = -1; f <= 1; f++)
+                for(int r = -1; r <= 1; r++)
+                    for(int length = 1; length<9 ; length++)
+                    if(r != 0 & f != 0)
+                    {
+                        var targetrank = bishop.Rank + r*length;
+                        var targetfile = bishop.File + f*length;
+                        if(targetrank >= 0 && targetrank <8 &&  targetfile>=0 && targetfile<8)
+                        {
+                            targetsquare = Board.GetSquareByRankAndFile(targetrank, targetfile);
+                            if (targetsquare.OccupingPiece!= "")
+                             {
+                                 if(targetsquare.OccupingPiece [0] != bishop.OccupingPiece[0])
+                                     LegalMoves.Add(new Move(bishop, targetsquare));
+                                break;
+                             }
+                             else
+                                 LegalMoves.Add(new Move(bishop, targetsquare));
+                        }
 
+                    }
         }
         private void CalculateKnightMoves(Square knight)
         {
-            
+            Square targetsquare;
+            for(int i = -2 ; i < 3; i ++)
+                for(int j = -2; j < 3 ; j++)
+                {
+                    if(i != 0 && j !=0 && i/j!=1 && i/j != -1)
+                    {
+                        var targetrank = knight.Rank + i;
+                        var targetfile = knight.File + j;
+                        if(targetrank >= 0 && targetrank < 8 && targetfile >= 0 && targetfile < 8)
+                        {
+                            targetsquare = Board.GetSquareByRankAndFile(targetrank, targetfile);
+                            if(targetsquare.OccupingPiece == "")
+                                LegalMoves.Add(new Move(knight,targetsquare));
+                            else
+                                if(targetsquare.OccupingPiece[0] != knight.OccupingPiece[0])
+                                    LegalMoves.Add(new Move(knight,targetsquare));
+                        }
+                            
+                    }
+                }
         }
         private void CalculateKingMoves(Square king)
         {
